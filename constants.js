49cb916a6c6ae8adc99b40e11a9100cf3f159662
ronan-f <rfegan13@gmail.com>
2021-06-07 22:47:44 -0700

    Added constants file

---
+++ constants.js
@@ -0,0 +1,10 @@
+export const pages = {
+    HOME: "/home",
+    LOGIN: "/login",
+    LOGOUT: "/logout",
+}
+
+export const auth = {
+    LOGIN: "/api/auth/login",
+    LOGOUT: "/api/auth/logout",
+}
