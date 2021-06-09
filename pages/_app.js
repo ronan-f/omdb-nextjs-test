67586f704b7339166e308839f63fc53c817fe4f1
Candice Pang <candicekhpang@hotmail.com.hk>
2021-06-08 19:03:02 -0700

    Added theme provider for app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import "../styles/globals.css"
-import { UserProvider } from "@auth0/nextjs-auth0"
+import { UserProvider } from "@auth0/nextjs-auth0";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import appTheme from '../styles/theme';
+
+const theme = createMuiTheme(appTheme);
 
 export default function App({ Component, pageProps }) {
     return (
         <UserProvider>
-            <Component {...pageProps} />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
         </UserProvider>
     )
 }
---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  palette: {
+    primary: {
+      main: '#829f9d',
+      contrast: '#ffffff',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+  },
+};
+
+export default theme;
