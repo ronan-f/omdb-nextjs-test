eae2bd908b52a50abd46001419cfe458b24bc8db
ronan-f <rfegan13@gmail.com>
2021-07-02 00:01:48 -0700

    Removed unused styling

--- components/Header.js
+++ components/Header.js
@@ -17,9 +17,6 @@ const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
     title: {
         flexGrow: 1,
     },
