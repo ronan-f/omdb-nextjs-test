b91b5e87f7e0452e1e202524c285740b7a71435c
ronan-f <rfegan13@gmail.com>
2021-09-26 16:49:31 -0700

    Replaced hardcoded loading

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import { useUser } from "@auth0/nextjs-auth0"
 import { useRouter } from "next/router"
 import { pages, auth } from "../constants"
+import { LoadingIndicator } from "../components/LoadingIndicator"
 
 export default function Main() {
     const router = useRouter()
 
     const { user, error, isLoading } = useUser()
     if (error) return <p>Oh no...{error.message}</p>
-    if (isLoading) return <p>Loading..</p>
+    if (isLoading) return <LoadingIndicator fullScreen />
 
     const destination = user ? pages.HOME : auth.LOGIN
 
