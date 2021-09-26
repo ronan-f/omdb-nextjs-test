4e84153572c1d0d22e4918dbacea09032482b54d
ronan-f <rfegan13@gmail.com>
2021-09-26 16:49:01 -0700

    Loaded actual data

--- components/ReviewFeed.js
+++ components/ReviewFeed.js
@@ -1,12 +1,26 @@
-import useGetReviews from '../hooks/useGetReviews'
-import ReviewCard from './ReviewCard'
+import { useEffect, useState } from "react"
+import useGetReviews from "../hooks/useGetReviews"
+import ReviewCard from "./ReviewCard"
+import { LoadingIndicator } from "./LoadingIndicator"
 
 const ReviewFeed = () => {
-    const reviews = useGetReviews();
+    const [reviews, setReviews] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        useGetReviews()
+            .then(setReviews)
+            .catch(console.error)
+            .finally(() => setLoading(false))
+    }, [])
+
+    if (loading) return <LoadingIndicator />
+    if (!reviews || !reviews.length)
+        return <h1>Hmm...no reviews to show yet.</h1>
 
     return reviews.map(({ id, ...props }) => {
         return <ReviewCard key={id} {...props} />
     })
 }
 
-export default ReviewFeed;
+export default ReviewFeed
