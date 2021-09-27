49b7d3aade3de8c2b995e2e6296f6e9444bcdf50
ronan-f <rfegan13@gmail.com>
2021-09-26 23:30:10 -0700

    Modified component to show user reviews

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,30 +1,36 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { Layout } from '../../components/Layout'
-import { useGetMovie } from '../../hooks/useGetMovie'
-import { LoadingIndicator } from '../../components/LoadingIndicator';
-import Movie from '../../components/Movie'
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import { Layout } from "../../components/Layout"
+import { useGetMovie } from "../../hooks/useGetMovie"
+import { LoadingIndicator } from "../../components/LoadingIndicator"
+import Movie from "../../components/Movie"
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
 
 const ReviewMovie = () => {
     const router = useRouter()
     const { id } = router.query
-    const [movie, setMovie] = useState(null);
+    const [movie, setMovie] = useState(null)
+    const [review, setReview] = useState(null)
 
     useEffect(() => {
-        if (!id) return;
+        if (!id) return
 
-        useGetMovie(id).then((res) => {
-            setMovie(res.data)
-        }).catch(console.error)
-    }, [id]);
+        useGetMovie(id)
+            .then((res) => {
+                console.log({ res })
+                setMovie(res.data.movie)
+                setReview(res.data.review)
+            })
+            .catch(console.error)
+    }, [id])
 
     if (!movie) return <LoadingIndicator fullScreen />
 
-    return <Layout>
-        <Movie {...movie} />
-    </Layout>
-
+    return (
+        <Layout>
+            <Movie review={review} {...movie} />
+        </Layout>
+    )
 }
 
-export default withPageAuthRequired(ReviewMovie);
+export default withPageAuthRequired(ReviewMovie)
