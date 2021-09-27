3ebb6785bd7d461aa072fe7992e0888b2f810ed2
ronan-f <rfegan13@gmail.com>
2021-09-26 23:27:53 -0700

    Updated component to post actual reviews

--- components/Movie.js
+++ components/Movie.js
@@ -1,41 +1,44 @@
-import { Card, Typography, CardContent, Box, Button } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import Rating from '@material-ui/lab/Rating';
-import TextField from '@material-ui/core/TextField';
+import { Card, Typography, CardContent, Box, Button } from "@material-ui/core"
+import { withStyles } from "@material-ui/core/styles"
+import Rating from "@material-ui/lab/Rating"
+import TextField from "@material-ui/core/TextField"
+import { useEffect, useState } from "react"
+import { useSetReview } from "../hooks/useSetReview"
+import Toast from "./Toast"
 
 const styles = {
     cardRoot: {
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        minWidth: '200px',
-        maxWidth: '875px',
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        minWidth: "200px",
+        maxWidth: "875px",
     },
     about: {
-        display: 'flex',
-        flexDirection: 'column',
-        width: '45%'
+        display: "flex",
+        flexDirection: "column",
+        width: "45%",
     },
     poster: {
-        height: '300px',
-        width: '200px',
+        height: "300px",
+        width: "200px",
     },
     cardContent: {
-        display: 'flex',
+        display: "flex",
     },
     userRating: {
-        width: '55%',
-        paddingLeft: '1rem'
+        width: "55%",
+        paddingLeft: "1rem",
     },
     title: {
-        color: 'white',
-        width: '100%',
-        padding: '0.5rem'
+        color: "white",
+        width: "100%",
+        padding: "0.5rem",
     },
     reviewInput: {
-        marginBottom: '1rem',
-    }
-};
+        marginBottom: "1rem",
+    },
+}
 
 const handleError = (e) => {
     const defaultImgLocation = "/assets/default-poster.jpeg"
@@ -45,54 +48,122 @@ const handleError = (e) => {
 
 const CriticRating = ({ source, value }) => {
     if (source === "Internet Movie Database") source = "IMDb"
-    return <>
-        <Typography>{source}: {value}</Typography>
-    </>
+    return (
+        <>
+            <Typography>
+                {source}: {value}
+            </Typography>
+        </>
+    )
 }
 
 const MovieRatings = ({ ratings }) => {
-
-    return ratings.map(rating => {
-        return <CriticRating key={rating.Source} source={rating.Source} value={rating.Value} />
-    })
-
-}
-
-const UserRatings = ({ classes }) => {
-    return <div className={classes.userRating}>
-        <div>
-
-            <Typography variant="subtitle1">Rate this movie</Typography>
-            <Rating precision={0.5} />
-        </div>
-        <div>
-            <Typography variant="subtitle1">Leave a review</Typography>
-            <TextField
-                className={classes.reviewInput}
-                id="standard-textarea"
-                placeholder="Oh what a fantastic movie..."
-                multiline
-                fullWidth
-                rows={10}
-                variant="outlined"
+    return ratings.map((rating) => {
+        return (
+            <CriticRating
+                key={rating.Source}
+                source={rating.Source}
+                value={rating.Value}
             />
-
-        </div>
-
-        <Button variant="contained" color="primary">Submit</Button>
-
-    </div>
+        )
+    })
 }
 
-const Movie = ({ Year, Title, Poster, Runtime, Ratings, Plot, ...props }) => {
-    const { classes } = props;
-    return (
+const UserRatings = ({
+    classes,
+    review,
+    rating,
+    setReview,
+    setRating,
+    movieId,
+    reviewId,
+    setShowSuccessToast,
+    setShowErrorToast,
+}) => {
+    const handleRatingChange = (e) => {
+        setRating(Number(e.target.value))
+    }
 
+    const handleReviewChange = (e) => {
+        setReview(e.target.value)
+    }
+
+    const handleSubmitReview = async () => {
+        setShowSuccessToast(true)
+
+        const res = await useSetReview(review, rating, movieId, reviewId)
+        if (!res) {
+            setShowErrorToast(true)
+            return
+        }
+    }
+    return (
+        <div className={classes.userRating}>
+            <div>
+                <Typography variant="subtitle1">Rate this movie</Typography>
+                <Rating
+                    id="rating"
+                    value={rating}
+                    onChange={handleRatingChange}
+                    precision={0.5}
+                />
+            </div>
+            <div>
+                <Typography variant="subtitle1">Leave a review</Typography>
+                <TextField
+                    className={classes.reviewInput}
+                    id="review"
+                    onChange={handleReviewChange}
+                    value={review}
+                    placeholder="Oh what a fantastic movie..."
+                    multiline
+                    fullWidth
+                    rows={10}
+                    variant="outlined"
+                />
+            </div>
+
+            <Button
+                onClick={handleSubmitReview}
+                variant="contained"
+                color="primary"
+            >
+                Submit
+            </Button>
+        </div>
+    )
+}
+
+const Movie = ({
+    Year,
+    Title,
+    Poster,
+    Runtime,
+    Ratings,
+    Plot,
+    review = {},
+    imdbID,
+    ...props
+}) => {
+    const { classes } = props
+
+    const [reviewContent, setReviewContent] = useState("")
+    const [rating, setRating] = useState(0)
+    const [showSuccessToast, setShowSuccessToast] = useState(false)
+    const [showErrorToast, setShowErrorToast] = useState(false)
+
+    useEffect(() => {
+        if (review) {
+            setReviewContent(review.content)
+            setRating(review.rating)
+        }
+    }, [review])
+
+    return (
         <Card className={classes.cardRoot} variant="outlined">
             <Box className={classes.title} bgcolor="primary.main">
                 <Typography variant="h3">{Title}</Typography>
             </Box>
-
 
             <CardContent className={classes.cardContent}>
                 <div className={classes.about}>
@@ -103,20 +174,42 @@ const Movie = ({ Year, Title, Poster, Runtime, Ratings, Plot, ...props }) => {
                         alt={`Movie poster for ${Title}`}
                     />
                     <div>
-                        <Typography color="textSecondary" variant="subtitle1">{Year} - {Runtime}</Typography>
+                        <Typography color="textSecondary" variant="subtitle1">
+                            {Year} - {Runtime}
+                        </Typography>
                         <Typography variant="subtitle2">{Plot}</Typography>
 
                         <MovieRatings ratings={Ratings} />
                     </div>
-
                 </div>
 
-                <UserRatings classes={classes} />
+                <UserRatings
+                    reviewId={review && review.id}
+                    movieId={imdbID}
+                    review={reviewContent}
+                    rating={rating}
+                    setReview={setReviewContent}
+                    setRating={setRating}
+                    classes={classes}
+                    setShowSuccessToast={setShowSuccessToast}
+                    setShowErrorToast={setShowErrorToast}
+                />
 
+                <Toast
+                    open={showSuccessToast}
+                    message="Review updated successfully"
+                    onClose={() => setShowSuccessToast(false)}
+                />
+
+                <Toast
+                    open={showErrorToast}
+                    state="error"
+                    message="Hmm. Something went wrong when saving. Try again."
+                    onClose={() => setShowErrorToast(false)}
+                />
             </CardContent>
-
         </Card>
     )
 }
 
-export default withStyles(styles)(Movie);
+export default withStyles(styles)(Movie)
