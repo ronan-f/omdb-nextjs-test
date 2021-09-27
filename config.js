d2c53e2c34a25047431383e1d548172ce57c88f3
ronan-f <rfegan13@gmail.com>
2021-09-27 00:51:09 -0700

    Loaded base url from env

--- config.js
+++ config.js
@@ -19,6 +19,4 @@ export const config = {
 
 const dev = process.env.NODE_ENV !== "production"
 
-export const server = dev
-    ? "http://localhost:3000"
-    : "https://omdb-nextjs-test.vercel.app"
+export const server = dev ? "http://localhost:3000" : process.env.BASE_URL
