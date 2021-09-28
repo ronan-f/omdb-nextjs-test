2eebac1a03a2f049ebf9d948d3285802cafe4004
ronan-f <rfegan13@gmail.com>
2021-09-27 17:02:06 -0700

    Handled local cache clearing by exporting global prisma

--- pages/api/_helpers/createPrismaClient.js
+++ pages/api/_helpers/createPrismaClient.js
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client"
 
-const client = new PrismaClient()
+let prisma
 
-export default client
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient()
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient()
+    }
+    prisma = global.prisma
+}
+
+export default prisma
