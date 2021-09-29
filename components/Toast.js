341553a204525ac8e3efacc5829e74a8372058b0
ronan-f <rfegan13@gmail.com>
2021-09-28 22:54:06 -0700

    Updated toast origin

--- components/Toast.js
+++ components/Toast.js
@@ -1,9 +1,19 @@
 import { Snackbar } from "@material-ui/core"
 import { Alert } from "@material-ui/lab"
+import isMobile from "../utils/isMobile"
 
 const Toast = ({ open, state = "success", message, onClose }) => {
+    let anchor = { vertical: "bottom", horizontal: "center" }
+    if (isMobile()) {
+        anchor.vertical = "top"
+    }
     return (
-        <Snackbar open={open} autoHideDuration={3000} onClose={onClose}>
+        <Snackbar
+            anchorOrigin={anchor}
+            open={open}
+            autoHideDuration={3000}
+            onClose={onClose}
+        >
             <Alert severity={state} sx={{ width: "100%" }}>
                 {message}
             </Alert>
